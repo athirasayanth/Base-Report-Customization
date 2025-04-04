87bc99d023239c1f7f3e299405253b53e14f1eaf
Athira Sayanth KV <athirakvsayanth@gmail.com>
2025-04-05 01:23:57 +0530

    Add files via upload

---
+++ cdx_invoice_basereport_customization/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Base Report Custom",
+    "version": "18.0",
+    "depends": ["account"],
+    "data": [
+        "views/view.xml",
+
+
+    ],
+    "installable": True,
+    "application": False,
+}
